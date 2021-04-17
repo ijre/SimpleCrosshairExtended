4c84769c7d4aa746d91bb1fb70b853155258a296
Paige Seivertson <pzeig66@yahoo.com>
2021-04-17 00:13:33 -0400

    Change formatting

--- Simple Crosshair Extended/SimpleCrosshair.lua
+++ Simple Crosshair Extended/SimpleCrosshair.lua
@@ -1,16 +1,17 @@
-SCrosshair = SCrosshair or { }
-SCrosshair.Ext = SCrosshair.Ext or { }
+SCrosshair = SCrosshair or
+{
+  Ext = { },
+  Enabled = true,
+  Visible = true
+}
 
 local base = ModPath
--- local base = ModPath .. "Simple CrossHair Extended/"
+-- local base = ModPath .. "Simple Crosshair Extended/"
 SCrosshair.Paths =
 {
   Base = base,
   Ext = base .. "Extensions/"
 }
-
-SCrosshair.Enabled = true
-SCrosshair.Visible = true
 
 function SCrosshair.Ext:ShouldShow()
   return SCrosshair.Enabled and SCrosshair.Visible
